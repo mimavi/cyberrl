da9954171a15e24f3049de74fc46b57d4a2965da
yiodhav <34611276+yiodhav@users.noreply.github.com>
2018-02-11 17:49:09 +0100

    placeholder random map

--- src/main.d
+++ src/main.d
@@ -3,6 +3,8 @@ import game;
 import actor;
 import std.stdio;
 import std.format;
+import std.random;
+import std.datetime;
 
 Game main_game;
 PlayerActor main_player;
@@ -13,10 +15,12 @@ void main()
 		import map;
 		main_game = new Game;
 		main_player = new PlayerActor(main_game.map, 10, 10);
-		main_game.map.get_tile(9, 9) = new WallTile;
-		main_game.map.get_tile(9, 11) = new WallTile;
-		main_game.map.get_tile(11, 9) = new WallTile;
-		main_game.map.get_tile(11, 11) = new WallTile;
+		auto rng = Random(cast(uint) Clock.currTime().fracSecs().total!"hnsecs");
+		foreach (i; 1..10000) {
+			int x = uniform(0, 300, rng);
+			int y = uniform(0, 300, rng);
+			main_game.map.get_tile(x, y) = new WallTile;
+		}
 		main_game.centerizeCamera(main_player.x, main_player.y);
 		main_game.run();
 	}
